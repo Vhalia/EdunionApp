55cf869e234abc50952b6e5449d427fa11c1d682
Vhalia <maxlegrelle@gmail.com>
2024-11-06 22:58:23 +0100

    add ruby script to copy Google Service file

---
+++ ios/addGoogleServiceFile.rb
@@ -0,0 +1,14 @@
+require ‘xcodeproj’
+
+project_path = ‘ProjectDir/EdunionApp.xcodeproj’
+
+project = Xcodeproj::Project.open(project_path)
+
+group = project.main_group["EdunionApp"]["EdunionApp"]
+
+file = group.new_file("GoogleService-Info.plist")
+
+main_target = project.targets.first
+main_target.add_file_references([file])
+
+project.save
