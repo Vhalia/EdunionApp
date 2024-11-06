e5226d0e5a6ad52fdba5a7dcb3bbdac800055b00
Vhalia <maxlegrelle@gmail.com>
2024-11-06 23:54:26 +0100

    fix script

--- ios/addGoogleServiceFile.rb
+++ ios/addGoogleServiceFile.rb
@@ -4,11 +4,32 @@ project_path = 'EdunionApp.xcodeproj'
 
 project = Xcodeproj::Project.open(project_path)
 
-group = project.main_group["EdunionApp"]
+target_name = 'EdunionApp'
+plist_file_path = 'GoogleService-Info.plist'
 
-file = group.new_file("GoogleService-Info.plist")
+target = project.targets.find { |t| t.name == target_name }
+unless target
+  puts "Target #{target_name} not found."
+  exit 1
+end
 
-#main_target = project.targets.first
-#main_target.add_file_references([file])
+# Check if the file already exists in the project
+file_ref = project.main_group.files.find { |file| file.path == plist_file_path }
+unless file_ref
+  file_ref = project.main_group.new_file(plist_file_path)
+end
+
+# Remove file from "Compile Sources" if it’s there
+compile_phase = target.source_build_phase
+if compile_phase.files_references.include?(file_ref)
+  compile_phase.remove_file_reference(file_ref)
+end
+
+# Add file to "Copy Bundle Resources" if it’s not already there
+copy_resources_phase = target.resources_build_phase
+unless copy_resources_phase.files_references.include?(file_ref)
+  copy_resources_phase.add_file_reference(file_ref)
+end
 
 project.save
+puts "GoogleService-Info.plist added to Copy Bundle Resources only."
