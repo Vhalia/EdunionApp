a5bfe0367e215d4a43d42a4e287d502acaa2489f
Vhalia <maxlegrelle@gmail.com>
2024-11-06 23:39:30 +0100

    fix ruby script

--- ios/addGoogleServiceFile.rb
+++ ios/addGoogleServiceFile.rb
@@ -8,7 +8,7 @@ group = project.main_group["EdunionApp"]
 
 file = group.new_file("GoogleService-Info.plist")
 
-main_target = project.targets.first
-main_target.add_file_references([file])
+#main_target = project.targets.first
+#main_target.add_file_references([file])
 
 project.save
