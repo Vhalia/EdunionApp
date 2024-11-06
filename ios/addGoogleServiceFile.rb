621c7f7eb373a5db26b055f703ac749065a8cfa6
Vhalia <maxlegrelle@gmail.com>
2024-11-06 23:07:13 +0100

    fix ruby script

--- ios/addGoogleServiceFile.rb
+++ ios/addGoogleServiceFile.rb
@@ -4,7 +4,7 @@ project_path = 'EdunionApp.xcodeproj'
 
 project = Xcodeproj::Project.open(project_path)
 
-group = project.main_group["EdunionApp"]["EdunionApp"]
+group = project.main_group["EdunionApp"]
 
 file = group.new_file("GoogleService-Info.plist")
 
